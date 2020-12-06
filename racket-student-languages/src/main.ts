36c43fbfbe7e13cb301c82b44705f7d499497cbf
Zibing Zhang <zibing.zha@gmail.com>
2020-12-06 16:33:36 -0500

    Pimped out IDE

--- racket-student-languages/src/main.ts
+++ racket-student-languages/src/main.ts
@@ -1,11 +1,35 @@
 import racket from './racket.js';
 
+let code = document.getElementById('code');
 let run = document.getElementById('run');
+
 if (run !== null) {
-  run.onclick = e => {
+  run.onclick = () => {
     console.clear();
     const text: string = document.getElementById('code')?.innerText || '';
     racket.run(text);
   };
   run.onclick(new MouseEvent(''));
 }
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Tab') {
+    e.preventDefault();
+    document.execCommand('insertHTML', false, '&#009');
+  }
+
+  if (!e.altKey) return;
+
+  switch (e.key) {
+    case 'c': {
+      if (code) code.innerHTML = ''; 
+      break;
+    }
+    case 'r': {
+      run?.click(); 
+      break;
+    }
+  }
+});
+
+code?.focus();
